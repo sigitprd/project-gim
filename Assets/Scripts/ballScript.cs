2d66ad339dfff8b0377a49632660a0686406cb98
sigitprd <18523125@students.uii.ac.id>
2020-03-27 18:52:04 +0700

    updated 27-03-2020

--- Assets/Scripts/ballScript.cs
+++ Assets/Scripts/ballScript.cs
@@ -6,9 +6,12 @@ public class ballScript : MonoBehaviour
 {
     // Start is called before the first frame update
     public int speed = 20;
+    public Rigidbody2D ball;
+
     void Start()
     {
-        GetComponent<Rigidbody2D>().velocity = new Vector2(1, -1) * speed;
+        //ball.constraints &= ~RigidbodyConstraints2D.FreezePositionY;
+        StartCoroutine(hitungMundurStart());
     }
 
     // Update is called once per frame
@@ -19,9 +22,28 @@ public class ballScript : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.collider.name == "WallLeft" || collision.collider.name == "WallRight")
+        
+        if (collision.collider.name == "WallLeft" || collision.collider.name == "WallRight")
         {
             GetComponent<Transform>().position = new Vector2(0, 0);
+            StartCoroutine(hitungMundurCollision());
         }
+
+    }
+
+    IEnumerator hitungMundurStart()
+    {
+        
+        yield return new WaitForSeconds(1);
+        //ball.constraints &= ~RigidbodyConstraints2D.None;
+        ball.velocity = new Vector2(1, -1) * speed;
+    }
+
+    IEnumerator hitungMundurCollision()
+    {
+        ball.velocity = Vector2.zero;
+        yield return new WaitForSeconds(1);
+        Start();
+        //ball.velocity = new Vector2(1, -1) * speed;
     }
 }
---
+++ Assets/Scripts/timerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timerScript : MonoBehaviour
+{
+    public int counter = 30;
+    public Text timerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerText.text = "Time: " + counter.ToString();
+        StartCoroutine(hitungMundur());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator hitungMundur()
+    {
+        while(counter > 0)
+        {
+            yield return new WaitForSeconds(2);
+            counter -= 1;
+            timerText.text = "Time: " + counter.ToString();
+        }
+
+        timerText.text = "GAME OVER!";
+    }
+}
