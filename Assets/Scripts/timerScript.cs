2bc05b2514c2c9e01842639c84a153ad1fb0f917
sigitprd <18523125@students.uii.ac.id>
2020-04-10 23:57:28 +0700

    update 10 april 2020

--- Assets/Scripts/timerScript.cs
+++ Assets/Scripts/timerScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class timerScript : MonoBehaviour
@@ -30,6 +31,7 @@ public class timerScript : MonoBehaviour
             timerText.text = "Time: " + counter.ToString();
         }
 
-        timerText.text = "GAME OVER!";
+        SceneManager.LoadScene("GameOverScene");
+        
     }
 }
